d4d8aed5159141cfb94fb9845e02ba66787a533a
avenx <psu.stoaven@gmail.com>
2016-05-20 15:26:52 +0800

    update

--- ngx_http_hello_module/ngx_http_hello_module.c
+++ ngx_http_hello_module/ngx_http_hello_module.c
@@ -69,131 +69,130 @@ ngx_module_t ngx_http_hello_module = {
 static ngx_int_t
 ngx_http_hello_handler(ngx_http_request_t *r)
 {
-        ngx_int_t    rc;
-        ngx_buf_t   *b;
-        ngx_chain_t  out;
-        ngx_http_hello_loc_conf_t* my_conf;
-        u_char ngx_hello_string[1024] = {0};
-        ngx_uint_t content_length = 0;
+    ngx_int_t    rc;
+    ngx_buf_t   *b;
+    ngx_chain_t  out;
+    ngx_http_hello_loc_conf_t* my_conf;
+    u_char ngx_hello_string[1024] = {0};
+    ngx_uint_t content_length = 0;
 
-        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "ngx_http_hello_handler is called!");
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "ngx_http_hello_handler is called!");
 
-        my_conf = ngx_http_get_module_loc_conf(r, ngx_http_hello_module);
-        if (my_conf->hello_string.len == 0 )
-        {
-                ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "hello_string is empty!");
-                return NGX_DECLINED;
-        }
+    my_conf = ngx_http_get_module_loc_conf(r, ngx_http_hello_module);
+    if (my_conf->hello_string.len == 0 )
+    {
+        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "hello_string is empty!");
+        return NGX_DECLINED;
+    }
 
-        if (my_conf->hello_counter == NGX_CONF_UNSET
-                || my_conf->hello_counter == 0)
-        {
-                ngx_sprintf(ngx_hello_string, "%s", my_conf->hello_string.data);
-        }
-        else
-        {
-                ngx_sprintf(ngx_hello_string, "%s Visited Times:%d", my_conf->hello_string.data,
-                        ++ngx_hello_visited_times);
-        }
-        ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "hello_string:%s", ngx_hello_string);
-        content_length = ngx_strlen(ngx_hello_string);
+    if (my_conf->hello_counter == NGX_CONF_UNSET
+            || my_conf->hello_counter == 0)
+    {
+  		ngx_sprintf(ngx_hello_string, "%s", my_conf->hello_string.data);
+    }
+    else
+    {
+        ngx_sprintf(ngx_hello_string, "%s Visited Times:%d", my_conf->hello_string.data,
+                ++ngx_hello_visited_times);
+    }
+	
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "hello_string:%s", ngx_hello_string);
+    content_length = ngx_strlen(ngx_hello_string);
 
-        /* we response to 'GET' and 'HEAD' requests only */
-        if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_HEAD))) {
-                return NGX_HTTP_NOT_ALLOWED;
-        }
+    /* we response to 'GET' and 'HEAD' requests only */
+    if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_HEAD))) {
+            return NGX_HTTP_NOT_ALLOWED;
+    }
 
-        /* discard request body, since we don't need it here */
-        rc = ngx_http_discard_request_body(r);
+    /* discard request body, since we don't need it here */
+    rc = ngx_http_discard_request_body(r);
 
-        if (rc != NGX_OK) {
-            return rc;
-        }
+    if (rc != NGX_OK) {
+        return rc;
+    }
 
-        /* set the 'Content-type' header */
-        /*
-         *r->headers_out.content_type.len = sizeof("text/html") - 1;
-         *r->headers_out.content_type.data = (u_char *)"text/html";
-         */
-        ngx_str_set(&r->headers_out.content_type, "text/html");
+    /* set the 'Content-type' header */
+    ngx_str_set(&r->headers_out.content_type, "text/html");
 
-
-        /* send the header only, if the request type is http 'HEAD' */
-        if (r->method == NGX_HTTP_HEAD) {
-                r->headers_out.status = NGX_HTTP_OK;
-                r->headers_out.content_length_n = content_length;
-
-                return ngx_http_send_header(r);
-        }
-
-        /* allocate a buffer for your response body */
-        b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
-        if (b == NULL) {
-            return NGX_HTTP_INTERNAL_SERVER_ERROR;
-        }
-
-        /* attach this buffer to the buffer chain */
-        out.buf = b;
-        out.next = NULL;
-
-        /* adjust the pointers of the buffer */
-        b->pos = ngx_hello_string;
-        b->last = ngx_hello_string + content_length;
-        b->memory = 1;    /* this buffer is in memory */
-        b->last_buf = 1;  /* this is the last buffer in the buffer chain */
-
-        /* set the status line */
+    /* send the header only, if the request type is http 'HEAD' */
+    if (r->method == NGX_HTTP_HEAD) {
         r->headers_out.status = NGX_HTTP_OK;
         r->headers_out.content_length_n = content_length;
 
-        /* send the headers of your response */
-        rc = ngx_http_send_header(r);
+        return ngx_http_send_header(r);
+    }
 
-        if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
-                return rc;
-        }
+    /* allocate a buffer for your response body */
+	b = ngx_create_temp_buf(r->pool, content_length);
+    if (b == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
 
-        /* send the buffer chain of your response */
-        return ngx_http_output_filter(r, &out);
+    /* attach this buffer to the buffer chain */
+    out.buf = b;
+    out.next = NULL;
+
+    /* adjust the pointers of the buffer */
+    b->pos = ngx_hello_string;
+    b->last = ngx_hello_string + content_length;
+    b->memory = 1;    /* this buffer is in memory */
+    b->last_buf = 1;  /* this is the last buffer in the buffer chain */
+
+    /* set the status line */
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = content_length;
+
+    /* send the headers of your response */
+    rc = ngx_http_send_header(r);
+
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+            return rc;
+    }
+
+    /* send the buffer chain of your response */
+    return ngx_http_output_filter(r, &out);
 }
 
 static void *ngx_http_hello_create_loc_conf(ngx_conf_t *cf)
 {
-        ngx_http_hello_loc_conf_t* local_conf = NULL;
-        local_conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_hello_loc_conf_t));
-        if (local_conf == NULL)
-        {
-                return NULL;
-        }
+    ngx_http_hello_loc_conf_t* hlcf = NULL;
 
-        ngx_str_null(&local_conf->hello_string);
-        local_conf->hello_counter = NGX_CONF_UNSET;
+	
+    hlcf = ngx_pcalloc(cf->pool, sizeof(ngx_http_hello_loc_conf_t));
+    if (hlcf == NULL)
+    {
+    	return NULL;
+    }
 
-        return local_conf;
+    ngx_str_null(&hlcf->hello_string);
+    hlcf->hello_counter = NGX_CONF_UNSET;
+
+    return hlcf;
 }
 
 /*
 static char *ngx_http_hello_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 {
-        ngx_http_hello_loc_conf_t* prev = parent;
-        ngx_http_hello_loc_conf_t* conf = child;
+    ngx_http_hello_loc_conf_t* prev = parent;
+    ngx_http_hello_loc_conf_t* conf = child;
 
-        ngx_conf_merge_str_value(conf->hello_string, prev->hello_string, ngx_hello_default_string);
-        ngx_conf_merge_value(conf->hello_counter, prev->hello_counter, 0);
+    ngx_conf_merge_str_value(conf->hello_string, prev->hello_string, ngx_hello_default_string);
+    ngx_conf_merge_value(conf->hello_counter, prev->hello_counter, 0);
 
-        return NGX_CONF_OK;
+    return NGX_CONF_OK;
 }*/
 
 static char *
 ngx_http_hello_string(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
-	ngx_http_hello_loc_conf_t* local_conf;
+	ngx_http_hello_loc_conf_t* hlcf = NULL;
+	char* rv = NULL;
 
 
-	local_conf = conf;
-	char* rv = ngx_conf_set_str_slot(cf, cmd, conf);
+	hlcf = conf;
+	rv = ngx_conf_set_str_slot(cf, cmd, conf);
 
-	ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "hello_string:%s", local_conf->hello_string.data);
+	ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "hello_string:%s", hlcf->hello_string.data);
 
 	return rv;
 }
@@ -202,37 +201,33 @@ ngx_http_hello_string(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 static char *ngx_http_hello_counter(ngx_conf_t *cf, ngx_command_t *cmd,
         void *conf)
 {
-        ngx_http_hello_loc_conf_t* local_conf;
-
-        local_conf = conf;
-
-        char* rv = NULL;
-
-        rv = ngx_conf_set_flag_slot(cf, cmd, conf);
+    ngx_http_hello_loc_conf_t* hlcf;
+    char* rv = NULL;
 
 
-        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "hello_counter:%d", local_conf->hello_counter);
-        return rv;
+    hlcf = conf;
+    rv = ngx_conf_set_flag_slot(cf, cmd, conf);
+
+    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "hello_counter:%d", hlcf->hello_counter);
+	
+    return rv;
 }
 
 static ngx_int_t
 ngx_http_hello_init(ngx_conf_t *cf)
 {
-        ngx_http_handler_pt        *h;
-        ngx_http_core_main_conf_t  *cmcf;
+    ngx_http_handler_pt        *h;
+    ngx_http_core_main_conf_t  *cmcf;
 
-		// push ngx_http_hello_handler in ngx_http_core_module
-		
-        cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
-		// call ngx_http_hello_handler  in phase NGX_HTTP_CONTENT_PHASE 
+    cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
-        h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
-        if (h == NULL) {
-                return NGX_ERROR;
-        }
+    h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
+    if (h == NULL) {
+            return NGX_ERROR;
+    }
 
-        *h = ngx_http_hello_handler;
+    *h = ngx_http_hello_handler;
 
-        return NGX_OK;
+    return NGX_OK;
 }
